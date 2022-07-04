845552ab10234ea8b95393217a185f1ad23ab619
Michal Orlik <sogehige@gmail.com>
2022-07-04 17:25:58 +0200

    feat: add url iframe overlay

---
+++ components/url.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe
+    frameBorder="0"
+    :src="props.opts.url"
+    width="100%"
+    height="100%"
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ opts: Object });
+
+onMounted(async () => {
+  console.log('====== URL ======');
+});
+</script>
