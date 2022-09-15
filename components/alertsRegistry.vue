29de6cb4f85e10785e3a45755f23ba05feb881bb
Michal Orlik <sogehige@gmail.com>
2022-09-15 16:20:22 +0200

    fix(alerts): fix layout3 positioning

--- components/alertsRegistry.vue
+++ components/alertsRegistry.vue
@@ -43,9 +43,7 @@
             >
               <div
                 v-if="runningAlert.event === 'promo' && runningAlert.user?.profileImageUrl"
-                :class="{
-                  center: runningAlert.alert.layout === '3',
-                }"
+                style="grid-column: 1; grid-row: 1;"
                 :style="{
                   'visibility': shouldAnimate ? 'visible' : 'hidden',
                 }"
@@ -66,9 +64,7 @@
               </div>
               <div
                 v-else-if="runningAlert.alert.imageId && typeOfMedia.get(runningAlert.alert.imageId) === 'video'"
-                :class="{
-                  center: runningAlert.alert.layout === '3',
-                }"
+                style="grid-column: 1; grid-row: 1;"
                 :style="{
                   'visibility': shouldAnimate ? 'visible' : 'hidden',
                 }"
@@ -96,9 +92,7 @@
               </div>
               <div
                 v-else-if="showImage"
-                :class="{
-                  center: runningAlert.alert.layout === '3',
-                }"
+                style="grid-column: 1; grid-row: 1;"
                 :style="{
                   'visibility': shouldAnimate ? 'visible' : 'hidden',
                 }"
@@ -120,9 +114,7 @@
               <div
                 id="text"
                 :key="String(runningAlert.isShowingText)"
-                :class="{
-                  center: runningAlert.alert.layout === '3',
-                }"
+                style="grid-column: 1; grid-row: 1; z-index: 9999;"
                 :style="{
                   'visibility': runningAlert.isShowingText ? 'visible' : 'hidden',
                   'width': fontWidth,
@@ -1007,6 +999,69 @@ onMounted(() => {
     }
   });
 
+  /* setInterval(() => {
+    console.log('Test alert');
+    const incomingAlert = {
+      amount:       73,
+      rewardId:     null,
+      name:         'AgitatedFast',
+      tier:         'Prime',
+      service:      'qiwi',
+      recipient:    'MuddySogeBot',
+      currency:     'EUR',
+      message:      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam dictum tincidunt diam. Aliquam erat volutpat. Mauris tincidunt sem sed arcu. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Praesent id justo in neque elementum ultrices. Integer pellentesque quam vel velit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Etiam commodo dui eget wisi. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      event:        'follows',
+      monthsName:   'měsíců',
+      isTTSMuted:   false,
+      isSoundMuted: false,
+      TTSService:   1,
+      TTSKey:       '75a917dc-d0a0-48a7-a4b5-62f430146a57',
+      user:         null,
+      caster:       {
+        userId:                    '96965261',
+        userName:                  'soge',
+        displayname:               'soge',
+        profileImageUrl:           'https://static-cdn.jtvnw.net/jtv_user_pictures/b963858f-f04d-4da9-ac10-56aa7308cec3-profile_image-300x300.png',
+        isOnline:                  true,
+        isVIP:                     false,
+        isModerator:               false,
+        isSubscriber:              true,
+        haveSubscriberLock:        false,
+        haveSubscribedAtLock:      false,
+        rank:                      '',
+        haveCustomRank:            false,
+        subscribedAt:              null,
+        seenAt:                    '2022-09-13T19:37:47.549Z',
+        createdAt:                 '2015-07-23T13:14:02.919Z',
+        watchedTime:               15949360748,
+        chatTimeOnline:            7453320000,
+        chatTimeOffline:           68690460000,
+        points:                    1355,
+        pointsOnlineGivenAt:       7453800000,
+        pointsOfflineGivenAt:      68690700000,
+        pointsByMessageGivenAt:    4769,
+        subscribeTier:             '3',
+        subscribeCumulativeMonths: 47,
+        subscribeStreak:           0,
+        giftedSubscribes:          0,
+        messages:                  4784,
+        extra:                     {
+          levels: {
+            xp:                '{"dataType":"BigInt","value":"38356"}',
+            xpOfflineGivenAt:  7453320000,
+            xpOfflineMessages: 0,
+            xpOnlineGivenAt:   7452810000,
+            xpOnlineMessages:  1,
+          },
+          theme: 'dark',
+        },
+      },
+      recipientUser: null,
+    };
+    processIncomingAlert(incomingAlert as any);
+  }, 5000);
+  */
+
   getSocket('/registries/alerts', true).on('alert', (data2) => {
     processIncomingAlert(data2);
   });
@@ -1316,7 +1371,8 @@ const encodeFont = (font: string) => {
   }
 
   .layout-3 {
-    display: block !important;
+    display: grid !important;
+    align-items: center;
   }
 
   .layout-4 {
