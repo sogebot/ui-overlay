ae67672dc0dd7a830e9ac2e27cfc77505bcfc34f
sogehige <sogehige@gmail.com>
2022-10-12 00:27:25 +0200

    feat(plugins): add plugins overlay (#312)

---
+++ components/pluginCustomOverlay.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app
+    v-if="$store.state.isUILoaded"
+    style="width: 100%; height: 100%;"
+  >
+    <v-main v-if="item" v-html="item.body || ''" />
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { getSocket } from '@sogebot/ui-helpers/socket';
+import safeEval from 'safe-eval';
+
+const props = defineProps({ opts: Object });
+const item = ref<null | { body: string, javascript: string }>(null);
+
+onMounted(() => {
+  getSocket('/core/plugins', true).emit('generic::getOne', props.opts?.pluginId, (err, plugin) => {
+    if (err || !plugin) {
+      return console.error(err || 'Plugin not found');
+    }
+    console.log(`=== PLUGIN ${plugin.id} LOADED ===`);
+    item.value = plugin;
+
+    try {
+      const workflow = JSON.parse(plugin.workflow).drawflow.Home.data;
+      for (const node of Object.values<any>(workflow)) {
+        const nodeId = node.data.value;
+        if (nodeId === props.opts?.nodeId) {
+          item.value = JSON.parse(node.data.data);
+
+          console.log(`==== NODE ${nodeId} FOUND ====`);
+
+          if ((item.value?.javascript || '').length > 0) {
+            (getSocket('/core/plugins', true) as any).emit(`plugins::getSandbox`, { nodeId, pluginId: plugin.id }, (sandbox: string) => {
+              safeEval(
+                item.value?.javascript || '', sandbox,
+              );
+            });
+          }
+
+          (getSocket('/core/plugins', true) as any).on(`plugins::${nodeId}::runScript`, ({ script, sandbox } : { script: string, sandbox: string }) => {
+            safeEval(
+              script, sandbox,
+            );
+          });
+          break;
+        }
+      }
+    } catch (e) {
+      console.log('Something went wrong.', e);
+    }
+  });
+});
+</script>
---
+++ pages/plugins/_pluginId/_nodeId/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <plugin-custom-overlay :opts="{ pluginId: route.params.pluginId, nodeId: route.params.nodeId }"/>
+</template>
+
+<script lang="ts">
+export default ({
+  middleware: ['isBotStarted'],
+  setup () {
+    const route = useRoute();
+    return { route };
+  },
+});
+</script>
